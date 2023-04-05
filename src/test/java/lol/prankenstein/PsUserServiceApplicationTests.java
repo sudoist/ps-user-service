0fd8942340936e8496563c459950b9cc1ad28954
John Cosio <johnc@mybizniche.com>
2023-04-05 22:59:58 +0800

    Initial commit

---
+++ src/test/java/lol/prankenstein/PsUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lol.prankenstein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
